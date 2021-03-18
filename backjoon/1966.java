e62f8b5404c94dfabc58e0a0e752a8aac2f3e1b9
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-18 15:29:21 +0900

    Create 1966.java

---
+++ backjoon/1966.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int count = 0;
+
+            Queue<int[]> queue = new LinkedList<>();
+            for(int j = 0; j < N; j++)
+                queue.offer(new int[] {j, sc.nextInt()});
+
+            while(!queue.isEmpty()) {
+                count++;
+                int max = queue.peek()[1];
+
+                for(int j = 0; j < queue.size(); j++) {
+                    int[] poll = queue.poll();
+                    if(max < poll[1]) max = poll[1];
+                    queue.offer(poll);
+                }
+
+                while(queue.peek()[1] != max) {
+                    queue.offer(queue.poll());
+                }
+
+                int[] delete = queue.poll();
+                if(delete[0] == M)
+                    sb.append(count).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
