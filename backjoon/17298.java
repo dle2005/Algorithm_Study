2ec4e2625dccbbb957bdd5501370e501cf6431d5
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-18 12:27:23 +0900

    Create 17298.java

---
+++ backjoon/17298.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++)
+            arr[i] = sc.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            while(!stack.empty() && arr[stack.peek()] < arr[i])
+                arr[stack.pop()] = arr[i];
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty())
+            arr[stack.pop()] = -1;
+
+        for(int i = 0; i < N; i++)
+            sb.append(arr[i]).append(' ');
+
+        System.out.println(sb);
+
+
+    }
+}
