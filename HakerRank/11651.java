5f23d2d97fa529609c3bb8aed0dbf9cf7b97e1ee
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-21 13:43:30 +0900

    Create 11651.java

---
+++ HakerRank/11651.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (x, y) -> {
+            if(x[1] == y[1])
+                return x[0] - y[0];
+            else
+                return x[1] - y[1];
+        });
+
+        for(int i = 0; i < N; i++)
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+    }
+}
