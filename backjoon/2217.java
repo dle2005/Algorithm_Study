b77d0303347c752c859e8edffdedb47f1b14c6d7
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-26 14:04:03 +0900

    Create 2217.java

---
+++ backjoon/2217.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++)
+            list.add(sc.nextInt());
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            if(max < list.get(i) * (i+1))
+                max = list.get(i) * (i+1);
+        }
+
+        System.out.println(max);
+    }
+}
