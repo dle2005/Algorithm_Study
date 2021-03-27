9dd586d7d8229502fda645e040c9a73f321fd679
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-27 14:18:21 +0900

    Create 9665.java

---
+++ backjoon/9665.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean turn = true;
+
+        while(n > 0) {
+            if(n >= 3)
+                n -= 3;
+            else
+                n-= 1;
+
+            if(turn)
+                turn = false;
+            else
+                turn = true;
+        }
+
+        if(turn)
+            System.out.println("CY");
+        else
+            System.out.println("SK");
+    }
+}
