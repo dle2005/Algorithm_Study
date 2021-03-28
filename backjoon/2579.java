4f915903714b0c6af5761349cb275a6b592cc68f
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-28 13:44:15 +0900

    Create 2579.java

---
+++ backjoon/2579.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] cost = new int[n+1];
+        int[] value = new int[n+1];
+
+        for(int i = 1; i <=n; i++)
+            cost[i] = sc.nextInt();
+
+        if(n == 1) {
+            System.out.println(cost[1]);
+            return;
+        }
+
+        value[1] = cost[1];
+        value[2] = cost[1] + cost[2];
+        for(int i = 3; i <=n; i++) {
+            value[i] = Math.max(value[i-2] + cost[i],
+                    cost[i-1] + value[i-3] + cost[i]);
+        }
+
+        System.out.println(value[n]);
+    }
+}
