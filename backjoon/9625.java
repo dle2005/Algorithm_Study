a7c4ed43ee947101bcc72082943ed0b6c5951443
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-03-29 10:58:46 +0900

    Create 9625.java

---
+++ backjoon/9625.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+        int[] A = new int[K + 1];
+        int[] B = new int[K + 1];
+
+        A[0] = 1; A[1] = 0;
+        B[0] = 0; B[1] = 1;
+
+        for(int i = 2; i <= K; i++) {
+            A[i] = A[i-2] + A[i-1];
+            B[i] = B[i-2] + B[i-1];
+        }
+
+        System.out.println(A[K] + " " + B[K]);
+    }
+}
