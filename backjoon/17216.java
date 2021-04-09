440429292645caf837650b476b2f5bfbe3a8539e
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-09 19:00:18 +0900

    Create 17216.java

---
+++ backjoon/17216.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N];
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            arr[i] = n;
+            dp[i] = n;
+
+            for(int j = 0; j < i; j++) {
+                if(arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+
+        }
+
+        int max = 0;
+        for(int i : dp)
+            max = Math.max(max, i);
+
+        System.out.println(max);
+    }
+}
