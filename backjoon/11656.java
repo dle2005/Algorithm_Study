0f788437f4d819c7477dcb7a96837e8c96c21bc8
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-22 10:14:49 +0900

    Create 11656.java

---
+++ backjoon/11656.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < S.length(); i++)
+            list.add(S.substring(i));
+
+        Collections.sort(list);
+
+        for(String t : list)
+            System.out.println(t);
+    }
+}
