5644bf05e929daba503b2a137ce70c6944891db4
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-25 21:39:21 +0900

    Create 10610.java

---
+++ backjoon/10610.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        if(!s.contains("0")) {
+            System.out.println(-1);
+            return;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++)
+            sum += s.charAt(i);
+
+        if(sum % 3 != 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+
+        String answer = "";
+
+        for(int i = s.length()-1; i >= 0; i--)
+            answer += arr[i];
+
+        System.out.println(answer);
+    }
+}
