cc15bc3045861f2c43e9cfee5b4cfd88cd01f642
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-26 19:29:12 +0900

    Create 2744.java

---
+++ backjoon/2744.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if('a' <= c && c <= 'z') sb.append((char)(c - 'a' + 'A'));
+            else sb.append((char)(c - 'A' + 'a'));
+        }
+
+        System.out.println(sb);
+    }
+}
