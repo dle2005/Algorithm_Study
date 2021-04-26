b69f5ec5c33bee4d68c320e69a375d07c3140b62
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-26 18:16:13 +0900

    Create 1357.java

---
+++ programmers/1357.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder x = new StringBuilder(st.nextToken());
+        StringBuilder y = new StringBuilder(st.nextToken());
+
+        x = x.reverse();
+        y = y.reverse();
+
+        int xy = Integer.parseInt(x.toString()) + Integer.parseInt(y.toString());
+
+        StringBuilder result = new StringBuilder(String.valueOf(xy));
+        result = result.reverse();
+
+        System.out.println(Integer.parseInt(result.toString()));
+    }
+}
