656714fa7ee6b1cb8b74b2d296b3f0ca0c153bb8
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-29 21:26:06 +0900

    Create 1454.java

---
+++ backjoon/1454.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        StringBuilder sb = new StringBuilder(s.substring(0, 1));
+
+        for (int i = 1; i < s.length(); i++) {
+            if(sb.charAt(i - 1) < s.charAt(i))
+                sb.insert(0, String.valueOf(s.charAt(i)));
+            else sb.append(s.charAt(i));
+        }
+
+        System.out.println(sb.reverse());
+    }
+}
