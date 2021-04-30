3970f1d7f9242b7d53b719719b04ccfc010f4cc0
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-30 18:39:32 +0900

    Create 1980.java

---
+++ backjoon/1980.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int a = Math.min(n, m);
+        int b = Math.max(n, m);
+
+        int max = t / a; // 18
+        int coke = t % a; // 1
+        int min = 0;
+
+        int burger = max;
+
+        while (max >= 0) {
+            int value = max * a + min * b;
+            int rest = t - value;
+
+            if (rest >= 0 && coke > rest) {
+                coke = rest;
+                burger = max + min;
+            }
+
+            max--;
+            if(value - a + b <= t) min++;
+        }
+
+        System.out.println(burger + " " + coke);
+    }
+}
