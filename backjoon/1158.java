1d9c6f721fa1f8c0e1bc42d817ec11e6d9dcecc7
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-04-30 10:05:17 +0900

    Create 1158.java

---
+++ backjoon/1158.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.*;
+
+public class B_1158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new LinkedList<>();
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for(int i = 1; i <= N; i++)
+            deque.offer(i);
+
+        for(int i = 0; i < N -1; i++) {
+            for(int j = 0; j < K - 1; j++)
+                deque.offer(deque.poll());
+
+            sb.append(deque.poll()).append(", ");
+        }
+        sb.append(deque.poll());
+
+        System.out.print("<");
+        System.out.print(sb);
+        System.out.print("> ");
+    }
+}
+
