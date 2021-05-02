5dbb9e7c97d406cec3870ed10af15cebea39879c
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-05-02 16:56:06 +0900

    Create 1159.java

---
+++ backjoon/1159.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] alp = new int[26];
+
+        for (int i = 0; i < N; i++)
+            alp[br.readLine().charAt(0) - 'a']++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if(alp[i] >= 5) sb.append((char) (i + 'a'));
+        }
+
+        if(sb.length() == 0) sb.append("PREDAJA");
+
+        System.out.println(sb);
+    }
+}
