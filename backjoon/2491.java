8bc33195d3f8e9e35001f58176b61195cf7ccaf1
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-05-04 17:48:21 +0900

    Create 2491.java

---
+++ backjoon/2491.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] increase = new int[N];
+        int[] decrease = new int[N];
+
+        Arrays.fill(increase, 1);
+        Arrays.fill(decrease, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int max = 1;
+
+        for (int i = 1; i < N; i++) {
+            if(arr[i-1] <= arr[i])
+                increase[i] = increase[i - 1] + 1;
+
+            max = Math.max(max, increase[i]);
+        }
+
+        for (int i = N - 2; i >= 0; i--) {
+            if(arr[i] >= arr[i+1])
+                decrease[i] = decrease[i+1] + 1;
+
+            max = Math.max(max, decrease[i]);
+        }
+
+        System.out.println(max);
+    }
+}
