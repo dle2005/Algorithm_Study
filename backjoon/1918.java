b12ff81ae28f8629b5b2a6f88d70e46ceb6fc494
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-05-05 13:13:08 +0900

    Create 1918.java

---
+++ backjoon/1918.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isAlphabetic(c)) {
+                sb.append(c);
+                continue;
+            }
+            else {
+                if (c == '(')
+                    stack.push(c);
+                else if (c == ')') {
+                    while (stack.peek() != '(')
+                        sb.append(stack.pop());
+                    stack.pop();
+                }
+                else if (c == '+' || c == '-') {
+                    while (!stack.isEmpty() && (stack.peek() == '+' || stack.peek() == '-' || stack.peek() == '*' || stack.peek() == '/'))
+                        sb.append(stack.pop());
+                    stack.push(c);
+                }
+                else if (c == '*' || c == '/') {
+                    while (!stack.isEmpty() && (stack.peek() == '*' || stack.peek() == '/'))
+                        sb.append(stack.pop());
+                    stack.push(c);
+                }
+            }
+        }
+
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
+
+        System.out.println(sb);
+    }
+}
