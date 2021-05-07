d129cc077a95d9da617d6b709ba69db8628571c0
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-05-07 11:07:05 +0900

    Create 3447.java

---
+++ backjoon/3447.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+
+            while(s.contains("BUG"))
+                s = s.replaceAll("(BUG)", "");
+
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
