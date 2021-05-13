469c3d137f30838c9b8779f8ebb4158d44f1a200
DongKonLee <55436258+dle2005@users.noreply.github.com>
2021-05-13 09:39:18 +0900

    Create 1094.java

---
+++ backjoon/1094.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+
+        int short_stick = 64;
+        int answer = 0;
+
+        while (X > 0) {
+            if(short_stick > X)
+                short_stick /= 2;
+            else {
+                answer++;
+                X -= short_stick;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
